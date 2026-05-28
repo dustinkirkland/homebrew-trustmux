3ecc80716722c1729a9b1074ec1522ea0e0b3324
Dustin Kirkland <kirkland@ubuntu.com>
2026-05-28 00:26:19 -0500

    trustmux: update to 7.0a12

--- Formula/trustmux.rb
+++ Formula/trustmux.rb
@@ -3,8 +3,8 @@ class Trustmux < Formula
 
   desc "Monitor and interact with tmux/Byobu sessions from your phone"
   homepage "https://trustmux.dev"
-  url "https://files.pythonhosted.org/packages/d7/61/9de5a0f6fecf4634c5491f059f5b8dc655f2a432f0716a4a316fe7daddfa/trustmux-7.0a11.tar.gz"
-  sha256 "8f50da4eca3add2c81edb3fd6c895791b51f237e8a0cca2f80a7f6dde81391b5"
+  url "https://files.pythonhosted.org/packages/c5/70/c3e233cad70ce6cda031f1e09a2d1308c1c7a159e9afeb6f0bb002d2b565/trustmux-7.0a12.tar.gz"
+  sha256 "ed11d84a72ad351842bd353b84702686b0617e7fdeb0b9cc82fa8706f1721070"
   license "GPL-3.0-or-later"
 
   depends_on "python@3.12"
